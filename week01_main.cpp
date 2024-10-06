2ff3cb84059f330663bc0766b20807040b998845
Toby Robinson <S4082261@student.rmit.edu.au>
2024-10-07 10:33:47 +1100

    adding

--- week01_main.cpp
+++ week01_main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <mcpp/mcpp.h>
-int main(void){
-// Create a Minecraft connection
-mcpp::MinecraftConnection mc;
-int build_x = 0;
-int build_y = 0;
-int build_z = 0;
-std::cout << "Enter the build coordinates [x y z]: " << std::endl;
-std::cin >> build_x;
-std::cin >> build_y;
-std::cin >> build_z;
-mcpp::Coordinate coord( build_x, build_y, build_z);
-mc.setBlock(coord, mcpp::Blocks::BRICKS);
+#include "Rectangle.h"
+int main(){
+Rectangle rect;
+rect.readRectangle();
+rect.buildRectangle();
 return EXIT_SUCCESS;
 }
